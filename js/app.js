9513eda9f3794c8e29dcb3aac2ad75de7e376a32
peterhendrick <peterjhendrick@gmail.com>
2016-10-16 01:52:00 -0600

    formatted app.js, no functional change

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Script Tutorials
  * http://www.script-tutorials.com/
  */
@@ -13,52 +13,52 @@
 'use strict';
 
 // angular.js main app initialization
-var app = angular.module('mileHighChauffeur', ['ngRoute', 'ngAnimate', 'ui.bootstrap']).
-    config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.
-        when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
-        when('/project/:projectId', {
-          templateUrl: function (params) { return 'pages/' + params.projectId + '.html'; },
-          controller: ProjectCtrl,
-          activetab: 'projects'
-        }).
-        when('/privacy', {
-          templateUrl: 'pages/privacy.html',
-          controller: PrivacyCtrl,
-          activetab: 'privacy'
-        }).
-        when('/about', {
-          templateUrl: 'pages/about.html',
-          controller: AboutCtrl,
-          activetab: 'about'
-        }).
-        otherwise({ redirectTo: '/' });
-    }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+var app = angular.module('mileHighChauffeur', ['ngRoute', 'ngAnimate', 'ui.bootstrap']).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/index.html',
+        activetab: 'projects',
+        controller: HomeCtrl
+    }).when('/project/:projectId', {
+        templateUrl: function (params) {
+            return 'pages/' + params.projectId + '.html';
+        },
+        controller: ProjectCtrl,
+        activetab: 'projects'
+    }).when('/privacy', {
+        templateUrl: 'pages/privacy.html',
+        controller: PrivacyCtrl,
+        activetab: 'privacy'
+    }).when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: AboutCtrl,
+        activetab: 'about'
+    }).otherwise({redirectTo: '/'});
+}]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
 
-        $scope.$on("$routeChangeSuccess", function (scope, next, current) {
-          $scope.part = $route.current.activetab;
+    $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+        $scope.part = $route.current.activetab;
+    });
+
+    // onclick event handlers
+    $scope.showForm = function () {
+        $('.contactRow').slideToggle();
+    };
+    $scope.closeForm = function () {
+        $('.contactRow').slideUp();
+    };
+
+    // save the 'Contact Us' form
+    $scope.save = function () {
+        $scope.loaded = true;
+        $scope.process = true;
+        $http.post('sendemail.php', $scope.message).success(function () {
+            $scope.success = true;
+            $scope.process = false;
         });
+    };
+}]);
 
-        // onclick event handlers
-        $scope.showForm = function () {
-          $('.contactRow').slideToggle();
-        };
-        $scope.closeForm = function () {
-          $('.contactRow').slideUp();
-        };
-
-        // save the 'Contact Us' form
-        $scope.save = function () {
-          $scope.loaded = true;
-          $scope.process = true;
-          $http.post('sendemail.php', $scope.message).success(function () {
-              $scope.success = true;
-              $scope.process = false;
-          });
-        };
-  }]);
-
-app.config(['$locationProvider', function($location) {
+app.config(['$locationProvider', function ($location) {
     $location.hashPrefix('!');
 }]);
 
