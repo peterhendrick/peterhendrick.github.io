9ef500c93dcecaa16102f47e67c8085a72713967
peterhendrick <peterjhendrick@gmail.com>
2016-10-16 01:35:53 -0600

    got injection working and updated to angular 1.5

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@
 'use strict';
 
 // angular.js main app initialization
-var app = angular.module('example359', []).
+var app = angular.module('example359', ['ngRoute', 'ngAnimate']).
     config(['$routeProvider', function ($routeProvider) {
       $routeProvider.
         when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
